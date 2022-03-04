415583099dc7459eb9e2d12891006ab687fa3092
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-04 15:41:04 +0100

    -i timer hanno pochi secondi (per fare le prove) / per ora non sto facendo la parte di css per concentrarmi sul funzionamento-

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const output = document.querySelector('.output');
+const outputTwo = document.querySelector('.output-two');
 let arr = [];
-let num; 
+let arrUser = [];
 
 // Visualizzare in pagina 5 numeri casuali.
 
 for (let i = 0; i < 5; i++) {
-    num = Math.floor(Math.random() * (100 - 1) + 1);
+    let num = Math.floor(Math.random() * (100 - 1) + 1);
     arr.push(num);
     output.innerHTML = `${arr}`;
 }
@@ -14,6 +15,23 @@ for (let i = 0; i < 5; i++) {
 // allo scadere dei quali li nascondete)
 // Dopo aver nascosto i numeri chiedete all'utente (con dei prompt) di inserirli in ordine, 
 // uno alla volta.
+
+setTimeout(hideNumbers, 3000);
+
+function hideNumbers() {
+    output.innerHTML = '';
+}
+
+setTimeout(guessNumbers, 5000);
+
+function guessNumbers() {
+    for (let j = 0; j < 5; j++) {
+        let userNumbers = parseInt(prompt('Inserisci i numeri che hai letto prima, uno alla volta'));
+        arrUser.push(userNumbers);
+    }
+    outputTwo.innerHTML = `${arrUser}`;
+}
+
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri 
 // da indovinare sono stati individuati.
 
