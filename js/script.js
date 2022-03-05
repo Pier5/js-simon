57ce195dabd2b74909a371ea1ae0446923c88ba1
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-05 13:44:16 +0100

    -ciclo while al posto del for-

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ for (let i = 0; i < 5; i++) {
 // Da lì parte un timer di 30 secondi. (Cioè lasciate i numeri visibili per 
 // 30 secondi  allo scadere dei quali li nascondete).
 
-setTimeout(hideNumbers, 30000);
+setTimeout(hideNumbers, 3000);
 
 function hideNumbers() {
     output.innerHTML = '';
@@ -34,18 +34,21 @@ function hideNumbers() {
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e
 // quali dei numeri  da indovinare sono stati individuati.
 
-setTimeout(guessNumbers, 31000);
+setTimeout(guessNumbers, 3100);
 
 function guessNumbers() {
-    for (let j = 0; j < arr.length; j++) {
+    let j = 0;
+    while (j < arr.length) {
         let userNumbers = parseInt(prompt('Inserisci i numeri nello stesso ordine in cui erano scritti, uno alla volta, altrimenti sarà contato come errore.'));
         arrUser.push(userNumbers);
         if (arr[j] == arrUser[j]) {
-            correctNumbers.push(arrUser[j]);
-        }   
+        correctNumbers.push(arrUser[j]);
+        totalNumbers = correctNumbers.length;
+        }    
+        j++;
         h2.innerHTML = 'Risultato';
         outputTwo.innerHTML = `Punteggio totale: ${totalNumbers} su 5`;
         outputThree.innerHTML = `Hai indovinato i seguenti numeri: ${correctNumbers}`;
-        totalNumbers = correctNumbers.length;
     }
 }
+
